50dc24eaf4c6881e6083336a82ea0a66b156ad8e
schnehowebking <schnehowebking@gmail.com>
2023-10-14 11:58:36 +0600

    Code Uploding

---
+++ Bank.py
@@ -0,0 +1,95 @@
+import random
+
+class Bank:
+    def __init__(self):
+        self.users = {}
+        self.bank_balance = 100000000
+        self.loan_status = True
+        self.loan_amount = 0
+
+    def create_user_account(self, name, email, address, account_type):
+        account_number = random.randint(10000, 99999)
+        while account_number in self.users:
+            account_number = random.randint(10000, 99999)
+
+        user = User(account_number, name, email, address, account_type)
+        self.users[account_number] = user
+        return user
+
+    def delete_user_account(self, account_number):
+        if account_number in self.users:
+            del self.users[account_number]
+            return True
+        return False
+
+    def see_all_user_accounts(self):
+        return self.users
+
+    def check_bank_balance(self):
+        return self.bank_balance
+
+    def check_total_loan_amount(self):
+        return self.loan_amount
+
+    def toggle_loan_feature(self):
+        self.loan_status = not self.loan_status
+
+    def take_loan(self, user, loan_amount):
+        if self.loan_status and self.bank_balance >= loan_amount and user.can_take_loan():
+            self.bank_balance -= loan_amount
+            self.loan_amount += loan_amount
+            user.take_loan(loan_amount)
+            return True
+        return False
+
+class User:
+    def __init__(self, account_number, name, email, address, account_type):
+        self.account_number = account_number
+        self.name = name
+        self.email = email
+        self.address = address
+        self.account_type = account_type
+        self.balance = 0
+        self.transactions = []
+        self.loan_count = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+        self.transactions.append(f"Deposited ${amount}")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Withdrawal amount exceeded"
+        else:
+            self.balance -= amount
+            self.transactions.append(f"Withdrew ${amount}")
+
+    def check_balance(self):
+        return self.balance
+
+    def check_transaction_history(self):
+        return self.transactions
+
+    def can_take_loan(self):
+        return self.loan_count < 2
+
+    def take_loan(self, loan_amount):
+        self.balance += loan_amount
+        self.loan_count += 1
+        self.transactions.append(f"Took a loan of ${loan_amount}")
+
+    def transfer_money(self, target_user, amount):
+        if target_user:
+            if amount > self.balance:
+                return "Insufficient balance for the transfer"
+            else:
+                self.balance -= amount
+                target_user.balance += amount
+                self.transactions.append(f"Transferred ${amount} to Account {target_user.account_number}")
+                target_user.transactions.append(f"Received ${amount} from Account {self.account_number}")
+        else:
+            return "Account does not exist"
+
+class Admin(User):
+    def __init__(self):
+        super().__init__(0, "Admin", "admin@bank.com", "Admin Address", "Admin")
---
+++ main.py
@@ -0,0 +1,137 @@
+from Bank import Bank, Admin, User
+bank = Bank()
+admin = Admin()
+
+while True:
+    print("\nBanking Management System:")
+    print("1. Admin Portal")
+    print("2. User Portal")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        admin_password = input("Enter admin password (admin): ")
+        if admin_password == "admin":
+            print("\nAdmin Menu:")
+            print("1. Create User Account")
+            print("2. Delete User Account")
+            print("3. See All User Accounts")
+            print("4. Check Bank Balance")
+            print("5. Check Total Loan Amount")
+            print("6. Turn Loan Feature On/Off")
+            print("7. Logout")
+            admin_option = input("Enter your choice: ")
+
+            if admin_option == "1":
+                name = input("Enter user's name: ")
+                email = input("Enter user's email: ")
+                address = input("Enter user's address: ")
+                account_type = input("Enter account type (Savings/Current): ")
+                account = bank.create_user_account(name, email, address, account_type)
+                print(f"Account created successfully. Account Number: {account.account_number}")
+            elif admin_option == "2":
+                account_number = int(input("Enter user's Account Number to delete: "))
+                if bank.delete_user_account(account_number):
+                    print(f"User Account {account_number} deleted successfully.")
+                else:
+                    print("User Account not found.")
+            elif admin_option == "3":
+                user_accounts = bank.see_all_user_accounts()
+                print("User Accounts:")
+                for account_number, user in user_accounts.items():
+                    print(f"Account Number: {account_number}, Name: {user.name}")
+            elif admin_option == "4":
+                print(f"Bank Balance: ${bank.check_bank_balance()}")
+            elif admin_option == "5":
+                print(f"Total Loan Amount: ${bank.check_total_loan_amount()}")
+            elif admin_option == "6":
+                bank.toggle_loan_feature()
+                status = "on" if bank.loan_status else "off"
+                print(f"Loan feature turned {status}.")
+            elif admin_option == "7":
+                print("Logged out.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+        else:
+            print("Admin login failed. Incorrect password.")
+    elif choice == "2":
+        user_option = input("1. Login\n2. Register\n3. Back to previous\nEnter your choice: ")
+        if user_option == "1":
+            account_number = int(input("Enter your account number: "))
+            user = bank.see_all_user_accounts().get(account_number)
+            if user:
+                print(f"Welcome, {user.name}!")
+                while True:
+                    print("\nUser Menu:")
+                    print("1. Deposit")
+                    print("2. Withdraw")
+                    print("3. Check Balance")
+                    print("4. Transaction History")
+                    print("5. Take a Loan")
+                    print("6. Transfer Money")
+                    print("7. Logout")
+                    user_option = input("Enter your choice: ")
+
+                    if user_option == "1":
+                        amount = float(input("Enter the amount to deposit: "))
+                        user.deposit(amount)
+                        print(f"${amount} deposited successfully.")
+                    elif user_option == "2":
+                        amount = float(input("Enter the amount to withdraw: "))
+                        result = user.withdraw(amount)
+                        if result == "Withdrawal amount exceeded":
+                            print(result)
+                        else:
+                            print(f"${amount} withdrawn successfully.")
+                    elif user_option == "3":
+                        print(f"Available balance: ${user.check_balance()}")
+                    elif user_option == "4":
+                        print("Transaction History:")
+                        for transaction in user.check_transaction_history():
+                            print(transaction)
+                    elif user_option == "5":
+                        if user.can_take_loan():
+                            loan_amount = float(input("Enter the loan amount: "))
+                            result = bank.take_loan(user, loan_amount)
+                            if result:
+                                print(f"Loan of ${loan_amount} granted successfully.")
+                            else:
+                                print("Loan service is currently unavailable or insufficient bank balance.")
+                        else:
+                            print("You have already taken the maximum number of loans (2).")
+                    elif user_option == "6":
+                        target_account = int(input("Enter the target account number: "))
+                        target_user = bank.see_all_user_accounts().get(target_account)
+                        if target_user:
+                            amount = float(input("Enter the amount to transfer: "))
+                            result = user.transfer_money(target_user, amount)
+                            if result == "Insufficient balance for the transfer":
+                                print(result)
+                            else:
+                                print(f"${amount} transferred successfully to Account {target_account}.")
+                        else:
+                            print("Account does not exist.")
+                    elif user_option == "7":
+                        print("Logged out.")
+                        break
+                    else:
+                        print("Invalid choice. Please try again.")
+            else:
+                print("Account not found. Please register.")
+        elif user_option == "2":
+            name = input("Enter your name: ")
+            email = input("Enter your email: ")
+            address = input("Enter your address: ")
+            account_type = input("Enter your account type (Savings/Current): ")
+            user = bank.create_user_account(name, email, address, account_type)
+            print(f"Account created successfully. Account Number: {user.account_number}")
+        elif user_option == "3":
+            continue
+        else:
+            print("Invalid choice. Please try again.")
+    elif choice == "3":
+        print("Thank you for using the Banking Management System.")
+        break
+    else:
+        print("Invalid choice. Please try again.")
